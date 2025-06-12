4f692acf1fe798be505ad52ef34a2767981a1055
Smz1n <samuel.gomesv2@gmail.com>
2025-06-12 03:01:53 -0300

    Feat: adicionando a alteração dos layouts dos avisos de exclusão das atividades

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Todo } from '../shared/models/todo.model';
 import { TodoService } from '../shared/services/todo.service';
 import { jsPDF } from 'jspdf';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-todo',
@@ -63,19 +64,40 @@ export class TodoComponent implements OnInit {
   }
 
   clearAll() {
-    if (this.todos.length > 0 && confirm('Are you sure you want to clear all tasks?')) {
+    if (this.todos.length === 0) return;
+
+
+    Swal.fire({
+    title: 'Tem certeza?',
+    text: 'Você deseja remover todas as tarefas?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Sim, limpar tudo!',
+    cancelButtonText: 'Cancelar'}).then((result) => {
+    if (result.isConfirmed) {
       this.todoService.clearAll();
       this.loadTodos();
     }
-  }
+  });
+}
 
-  clearCompletedTasks() {
-    if (this.todos.length > 0 && confirm('Are you sure you want to clear all completed tasks?')) {
-        this.todoService.clearCompletedTasks();
-        this.loadTodos();
+clearCompletedTasks() {
+  if (this.todos.length === 0) return;
+
+  Swal.fire({
+    title: 'Tem certeza?',
+    text: 'Você deseja remover todas as tarefas concluídas?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Sim, limpar concluídas!',
+    cancelButtonText: 'Cancelar'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      this.todoService.clearCompletedTasks();
+      this.loadTodos();
     }
-  }
-
+  });
+}
   toggleCompletedTasks() {
     this.showCompletedTasks = !this.showCompletedTasks;
     this.loadTodos();
